91158dfe911c7042e57cb475bb3b2f0df2911909
Akhil V N <akhil.vn@peppo.co.in>
2021-06-20 03:23:47 +0530

    a

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome Kesla
+          Welcome Kesla 
         </h1>
       </main>
 
